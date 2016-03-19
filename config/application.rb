6c97fe75f2b90a9a6229875239070aa5ce7968a7
= <derronbrown@gmail.com>
2016-03-19 12:08:02 -0500

    Integration of s3

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,8 @@ module TestX
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #added for heroku deploy
+    config.assets.initialize_on_precompile = false
   end
 end
